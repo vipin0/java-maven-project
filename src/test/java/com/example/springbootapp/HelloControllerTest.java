84d8043c5f7784154c552887ed6c9734a67291f7
vipin0 <vipin6673@gmail.com>
2021-11-18 11:13:33 +0530

    changed

--- src/test/java/com/example/springbootapp/HelloControllerTest.java
+++ src/test/java/com/example/springbootapp/HelloControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
